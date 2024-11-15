17e52f602f03df6f0f897dd3786f79881f6458f0
dustisme <androsrivas@outlook.com>
2024-11-15 09:58:41 +0100

    implement services, hooks and context to card component

--- src/components/molecules/Card/Card.jsx
+++ src/components/molecules/Card/Card.jsx
@@ -6,18 +6,24 @@ function Card() {
   const { cards } = useContext(CardContext);
   
   return (
-    <div className="card">
-        <div className="img-container">
-            <img className="card-img" src="" alt="" />
+    <>
+      {
+        cards.map((card) => {
+          <div className="card">
+            <div className="img-container">
+                <img className="card-img" src={ card.imageUrl } alt={ card.name } />
+            </div>
+            <section className="card-info">
+                <h3>{ card.name}</h3>
+                <h4>Type</h4>
+                <p>{ card.type }</p>
+                <h4>Description</h4>
+                <p>{ card.originaltext }</p>
+            </section>
         </div>
-        <section className="card-info">
-            <h3>Card name</h3>
-            <h4>Type</h4>
-            <p>Card type</p>
-            <h4>Description</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, molestiae optio. Corrupti optio quisquam aliquid quasi quaerat error, amet nemo iusto ipsam asperiores! Error quia maxime inventore quas. Soluta, sint?</p>
-        </section>
-    </div>
+        })
+      }
+    </>
   )
 }
 
