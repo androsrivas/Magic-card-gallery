84fe95dd3d934b9715c7fd7b78ac05cedfdb66a9
dustisme <androsrivas@outlook.com>
2024-11-15 11:56:50 +0100

    fix card component renderization

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,24 +1,27 @@
 import { useContext } from "react";
 import { CardContext } from "../context/CardContext/CardContext";
 import Card from "../components/molecules/Card/Card";
-
+import "./Cards.css";
 
 const Cards = () => {
     const { cards } = useContext(CardContext);
     return (
-        <>
+        <main className="main-container">
+            <h2 className="title">Cartas</h2>
             {
-                cards.map((card) => {
-                    <Card 
-                        imageUrl={  card.imageUrl }
-                        name={ card.name }
-                        type={ card.type }
-                        originaltext={ card.originaltext }
-                    />
-                })
+                cards.map((card) => (
+                    <div>
+                        <Card 
+                            imageUrl={ card.imageUrl }
+                            name={ card.name }
+                            type={ card.type }
+                            text={ card.text }
+                        />
+                    </div>
+                ))
             }
-        </>    
-  )
+        </main>    
+    )
 }
 
 export default Cards
